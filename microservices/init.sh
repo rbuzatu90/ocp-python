380d57c758ce5211a6b4c6556e384fffa1652ce4
Remus Buzatu <r.buzatu90@gmail.com>
2023-11-20 07:40:10 -0500

    Switch to DeployConfig and BuildConfig

--- microservices/init.sh
+++ microservices/init.sh
@@ -29,7 +29,7 @@ EOF'
 }
 
 
-#oc adm policy add-scc-to-user privileged -z default
+#oc adm policy add-scc-to-user privileged -z default -n remus
 #oc adm policy add-scc-to-user anyuid -z default -n remus
 
 
