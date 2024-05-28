6fe208243632f8b1c5e00126a41319b31d6667c1
Remus Buzatu <r.buzatu90@gmail.com>
2024-05-28 11:07:41 +0300

    OCP NFS deployment

--- ocp-deploy/post/nfs/nfs-deploy.sh
+++ ocp-deploy/post/nfs/nfs-deploy.sh
@@ -1,3 +1,7 @@
 oc new-project nfs
 oc create -f rbac.yaml
 oc adm policy add-scc-to-user hostmount-anyuid system:serviceaccount:nfs:nfs-client-provisioner
+oc apply -f deployment.yaml
+oc apply -f class.yaml
+oc apply -f test-claim.yaml
+oc apply -f test-pod.yaml
